322f56b33eb41bdb184c1d1d7406ea85ddf4f3b0
ValeriaS277825 <77066053+ValeriaS277825@users.noreply.github.com>
2021-01-06 17:33:18 +0100

    Create Prova.py

---
+++ Prova.py
@@ -0,0 +1,17 @@
+class Frutta:
+  def _init_(self, frutta):
+    self.frutta = frutta
+
+    #test o next, è solo il nome della funzione attuale, purchè poi lo cambio anche giu
+  def test(self):
+    myit = iter(self.frutta)
+    print(next(myit))
+    print(next(myit))
+    print(next(myit))
+    print(next(myit))
+    print(next(myit))
+    print(next(myit))
+
+pippo = Frutta("banana")
+#test o next
+pippo.test()
